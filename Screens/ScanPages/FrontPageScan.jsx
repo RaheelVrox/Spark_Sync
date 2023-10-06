cb4036b11fe82441a763b0d7c90db500ca797fe3
RaheelVrox <raheel@vrox.co.uk>
2023-10-06 18:20:56 +0500

    textocr detect

--- Screens/ScanPages/FrontPageScan.jsx
+++ Screens/ScanPages/FrontPageScan.jsx
@@ -1,11 +1,21 @@
-// import React, { useState, useEffect, useRef } from 'react';
-// import { View, Text, TouchableOpacity, Image, PermissionsAndroid, Platform } from 'react-native';
-// import TextDetector from 'react-native-text-detector';
+// import React, { useState, useEffect, useRef } from "react";
+// import {
+//   View,
+//   Text,
+//   TouchableOpacity,
+//   Image,
+//   PermissionsAndroid,
+//   Platform,
+// } from "react-native";
 // import { Camera } from "expo-camera";
+// import TesseractOcr, {
+//   LANG_ENGLISH,
+//   LEVEL_WORD,
+// } from "react-native-tesseract-ocr";
 
 // const FrontPageScan = () => {
 //   const [imageUri, setImageUri] = useState(null);
-//   const [detectedText, setDetectedText] = useState('');
+//   const [detectedText, setDetectedText] = useState("");
 //   const cameraRef = useRef(null);
 
 //   useEffect(() => {
@@ -13,19 +23,19 @@
 //   }, []);
 
 //   const requestCameraPermission = async () => {
-//     if (Platform.OS === 'android') {
+//     if (Platform.OS === "android") {
 //       try {
 //         const granted = await PermissionsAndroid.request(
 //           PermissionsAndroid.PERMISSIONS.CAMERA,
 //           {
-//             title: 'Camera Permission',
-//             message: 'App needs access to your camera.',
+//             title: "Camera Permission",
+//             message: "App needs access to your camera.",
 //           }
 //         );
 //         if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-//           console.log('Camera permission granted');
+//           console.log("Camera permission granted");
 //         } else {
-//           console.log('Camera permission denied');
+//           console.log("Camera permission denied");
 //         }
 //       } catch (err) {
 //         console.warn(err);
@@ -42,21 +52,33 @@
 //     }
 //   };
 
+//   // const detectText = async (uri) => {
+//   //   try {
+//   //     const ocrResult = await TesseractOcr?.recognize(uri, LANG_ENGLISH, {
+//   //       level: LEVEL_WORD,
+//   //     });
+//   //     setDetectedText(ocrResult);
+//   //   } catch (e) {
+//   //     console.error(e);
+//   //   }
+//   // };
+
 //   const detectText = async (uri) => {
+//     console.log("uri", uri);
 //     try {
-//       const visionResp = await TextDetector.detectFromUri(uri);
-//       let text = '';
-//       for (let i = 0; i < visionResp.length; i++) {
-//         text += visionResp[i].text + ' ';
-//       }
-//       setDetectedText(text);
+//       console.log("Detecting text...");
+//       const ocrResult = await TesseractOcr?.recognize(uri, LANG_ENGLISH, {
+//         level: LEVEL_WORD,
+//       });
+//       console.log("OCR Result:", ocrResult); // Log the OCR result
+//       setDetectedText(ocrResult);
 //     } catch (e) {
-//       console.error(e);
+//       console.error("Error in text detection:", e);
 //     }
 //   };
 
 //   return (
-//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
 //       <Camera
 //         ref={cameraRef}
 //         style={{ width: 300, height: 300 }}
@@ -65,189 +87,120 @@
 //       <TouchableOpacity onPress={takePicture} style={{ marginTop: 10 }}>
 //         <Text>Capture Image</Text>
 //       </TouchableOpacity>
-//       {imageUri && <Image source={{ uri: imageUri }} style={{ width: 200, height: 200, marginTop: 20 }} />}
-//       {detectedText !== '' && (
-//         <Text style={{ marginTop: 20 }}>
-//           Detected Text: {detectedText}
-//         </Text>
+//       {imageUri && (
+//         <Image
+//           source={{ uri: imageUri }}
+//           style={{ width: 200, height: 200, marginTop: 20 }}
+//         />
+//       )}
+//       {detectedText !== "" && (
+//         <Text style={{ marginTop: 20 }}>Detected Text: {detectedText}</Text>
 //       )}
 //     </View>
 //   );
 // };
 // export default FrontPageScan;
 
-import React, { useState, useEffect, useRef } from "react";
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  Image,
-  PermissionsAndroid,
-  Platform,
-} from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, TouchableOpacity, Image } from "react-native";
 import { Camera } from "expo-camera";
-import TesseractOcr, {
-  LANG_ENGLISH,
-  LEVEL_WORD,
-} from "react-native-tesseract-ocr";
+import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
+import TesseractOcr, { LANG_ENGLISH } from "react-native-tesseract-ocr";
 
 const FrontPageScan = () => {
-  const [imageUri, setImageUri] = useState(null);
+  const [isCameraOpen, setIsCameraOpen] = useState(false);
   const [detectedText, setDetectedText] = useState("");
-  const cameraRef = useRef(null);
+  const [selectedImage, setSelectedImage] = useState(null);
 
   useEffect(() => {
-    requestCameraPermission();
+    // Initialize TesseractOcr when the app starts
+    initializeTesseract();
   }, []);
 
-  const requestCameraPermission = async () => {
-    if (Platform.OS === "android") {
-      try {
-        const granted = await PermissionsAndroid.request(
-          PermissionsAndroid.PERMISSIONS.CAMERA,
-          {
-            title: "Camera Permission",
-            message: "App needs access to your camera.",
-          }
-        );
-        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-          console.log("Camera permission granted");
-        } else {
-          console.log("Camera permission denied");
-        }
-      } catch (err) {
-        console.warn(err);
-      }
+  const initializeTesseract = async () => {
+    const { status } = await Permissions.askAsync(
+      Permissions.CAMERA,
+      Permissions.MEDIA_LIBRARY
+    );
+
+    if (status === "granted") {
+      TesseractOcr.initialize();
+    } else {
+      console.warn("Camera and media library permissions not granted.");
     }
   };
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      if (status !== "granted") {
+        alert("Camera permission not granted");
+      }
+
+      const { status: mediaStatus } =
+        await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if (mediaStatus !== "granted") {
+        alert("Media Library permission not granted");
+      }
+    })();
+  }, []);
 
   const takePicture = async () => {
-    if (cameraRef.current) {
-      const options = { quality: 0.5, base64: true };
-      const data = await cameraRef.current.takePictureAsync(options);
-      setImageUri(data.uri);
-      detectText(data.uri);
+    if (!isCameraOpen) return;
+
+    const photo = await ImagePicker.launchCameraAsync({});
+
+    if (!photo.canceled) {
+      setSelectedImage(photo.uri);
+      recognizeText(photo.uri);
     }
   };
 
-  const detectText = async (uri) => {
+  const recognizeText = async (imageUri) => {
     try {
-      const ocrResult = await TesseractOcr.recognize(uri, LANG_ENGLISH, {
-        level: LEVEL_WORD,
-      });
-      setDetectedText(ocrResult);
-    } catch (e) {
-      console.error(e);
+      const result = await TesseractOcr.recognize(imageUri, LANG_ENGLISH);
+      setDetectedText(result || "No text detected");
+    } catch (error) {
+      console.error(error);
     }
   };
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Camera
-        ref={cameraRef}
-        style={{ width: 300, height: 300 }}
-        captureAudio={false}
-      />
-      <TouchableOpacity onPress={takePicture} style={{ marginTop: 10 }}>
-        <Text>Capture Image</Text>
-      </TouchableOpacity>
-      {imageUri && (
-        <Image
-          source={{ uri: imageUri }}
-          style={{ width: 200, height: 200, marginTop: 20 }}
-        />
-      )}
-      {detectedText !== "" && (
-        <Text style={{ marginTop: 20 }}>Detected Text: {detectedText}</Text>
+    <View style={{ flex: 1 }}>
+      {isCameraOpen ? (
+        <Camera
+          style={{ flex: 1, justifyContent: "center" }}
+          type={Camera.Constants.Type.back}
+        >
+          <TouchableOpacity onPress={takePicture}>
+            <View>
+              <Text
+                style={{ color: "#fff", alignSelf: "center", fontSize: 16 }}
+              >
+                Take Picture
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </Camera>
+      ) : (
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          {selectedImage && (
+            <Image
+              source={{ uri: selectedImage }}
+              style={{ width: 200, height: 200 }}
+            />
+          )}
+          <Text>{detectedText}</Text>
+          <TouchableOpacity onPress={() => setIsCameraOpen(true)}>
+            <Text>Open Camera</Text>
+          </TouchableOpacity>
+        </View>
       )}
     </View>
   );
 };
+
 export default FrontPageScan;
-
-// import React, { useState } from 'react';
-// import { View, Text, TouchableOpacity, Image, PermissionsAndroid } from 'react-native';
-// import ImagePicker from 'react-native-image-picker';
-// import TesseractOcr, { LANG_ENGLISH } from 'react-native-tesseract-ocr';
-
-// const FrontPageScan = () => {
-//   const [imagePath, setImagePath] = useState('');
-//   const [extractedText, setExtractedText] = useState('');
-
-//   const requestCameraPermission = async () => {
-//     try {
-//       const granted = await PermissionsAndroid.request(
-//         PermissionsAndroid.PERMISSIONS.CAMERA,
-//         {
-//           title: 'Camera Permission',
-//           message: 'This app needs access to your camera to take pictures.',
-//         }
-//       );
-//       if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-//         console.log('Camera permission granted');
-//       } else {
-//         console.log('Camera permission denied');
-//       }
-//     } catch (err) {
-//       console.warn(err);
-//     }
-//   };
-
-//   const openCamera = async () => {
-//     await requestCameraPermission();
-
-//     const options = {
-//       storageOptions: {
-//         skipBackup: true,
-//         path: 'images',
-//       },
-//     };
-
-//     ImagePicker.launchCamera(options, (response) => {
-//       if (response.didCancel) {
-//         console.log('User cancelled image picker');
-//       } else if (response.error) {
-//         console.log('ImagePicker Error: ', response.error);
-//       } else {
-//         setImagePath(response.uri);
-//         recognizeText(response.path);
-//       }
-//     });
-//   };
-
-//   const recognizeText = async (imagePath) => {
-//     try {
-//       const recognizedText = await TesseractOcr.recognize(
-//         imagePath,
-//         LANG_ENGLISH,
-//         {},
-//       );
-//       setExtractedText(recognizedText);
-//     } catch (err) {
-//       console.error(err);
-//     }
-//   };
-
-//   return (
-//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//       {imagePath ? (
-//         <Image source={{ uri: imagePath }} style={{ width: 200, height: 200 }} />
-//       ) : (
-//         <Text>No image selected</Text>
-//       )}
-
-//       <TouchableOpacity onPress={openCamera}>
-//         <Text>Select Image from Camera</Text>
-//       </TouchableOpacity>
-
-//       {extractedText ? (
-//         <View style={{ marginTop: 20 }}>
-//           <Text>Extracted Text:</Text>
-//           <Text>{extractedText}</Text>
-//         </View>
-//       ) : null}
-//     </View>
-//   );
-// };
-
-// export default FrontPageScan;
